44d741f8abced28d457125631a3347a6e6057e3c
yuval-belelovsky <ybelelovsky@campus.technion.ac.il>
2023-05-08 16:16:48 +0300

    added documentation

--- data.py
+++ data.py
@@ -1,11 +1,16 @@
 import pandas
 
+"""
+    reads the data from the file and puts it in a dictionary with the necessary features
+"""
 def load_data(path,features):
     df = pandas.read_csv(path)
     data = df.to_dict(orient="list")
     data = {x:data[x] for x in features}
     return data
-
+"""
+    seperates the dictionary into two dictionaries by the value of each feature
+"""
 def filter_by_feature(data, feature, values):
     data1 = data.copy()
     data2 = data.copy()
@@ -18,19 +23,26 @@ def filter_by_feature(data, feature, values):
                 data1[k].remove(data[k][i])
     return data1,data2
 
+"""
+    prints the value of the statistic methods for each feature
+"""
 def print_details(data, features, statistic_functions):
     temp = []
     for feature in features:
         for y in statistic_functions:
-            temp.append(y(x))
+            temp.append(y(data[feature]))
         print(feature,": ", ','.join(temp))
         temp.clear()
     return()
 
+"""
+    prints the value of the statistic methods for for two features
+"""
 
 def print_joint_details(data, features, statistic_functions, statistic_functions_names):
     for function in statistic_functions:
-        print(statistic_functions_names[function],"(",features[1],",",features[2],"): ",function(features[1],features[2]))
+        print(statistic_functions_names[function],"(",features[1],",",features[2],"): ",
+              function(data[features[1]],data[features[2]]))
     return()
 
 
